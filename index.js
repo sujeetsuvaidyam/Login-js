dd8aa9b7c59207acd31d1f62818943de1299c5b9
sujeetsuvaidyam <sujeet.suvaidyam@gmail.com>
2022-11-23 12:50:27 +0530

    modified js

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ function login() {
     let username = document.getElementById("name").value;
     let pass = document.getElementById("Password").value;
 
-    
-    if (username=="Sujeet" && pass=="321") {
+
+    if (username == "Sujeet" && pass == "321") {
         location.assign("http://127.0.0.1:5501/index.html");
-    } else {
+    }
+    else if (username == "Sujeet" && pass != "") {
+        alert("Invailed Password");
+    }
+    else if (username != "" && pass == "321") {
+        alert("Invailed username");
+    }
+    else if (username != "" && pass != "") {
+        alert("Invailed username and  Password");
+    } else if (username != "") {
+        alert("Invailed username");
+    }
+    else if (pass != "") {
+        alert("Invailed Password")
+    }
+    else {
         alert("Invailed login");
-    };
+    }
 
     let valuepass = {
         Name: username,
