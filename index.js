89ad51f2913548b84ffc6037a248f3bdce38e334
sujeetsuvaidyam <sujeet.suvaidyam@gmail.com>
2022-11-23 11:42:09 +0530

    modified css

---
+++ index.js
@@ -0,0 +1,21 @@
+let data = [];
+
+function login() {
+    let username = document.getElementById("name").value;
+    let pass = document.getElementById("Password").value;
+
+    
+    if (username=="Sujeet" && pass=="321") {
+        location.assign("http://127.0.0.1:5501/");
+    } else {
+        alert("Invailed login");
+    };
+
+    let valuepass = {
+        Name: username,
+        password: pass,
+    };
+    data.push(valuepass);
+    //  console.log(data)
+
+};
